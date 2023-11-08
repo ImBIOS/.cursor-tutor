78006c17218913b87a6d967e236d5312a21bac91
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-11-08 07:13:10 +0700

    update

--- projects/javascript/src/index.js
+++ projects/javascript/src/index.js
@@ -6,7 +6,7 @@
 Step 1: Try generating a react component that lets you play tictactoe with Cmd+K or Ctrl+K on a new line.
   - Then integrate it into the code below and run with npm start
 
-Step 2: Try highlighting all the code with your mouse, then hit Cmd+k or Ctrl+K. 
+Step 2: Try highlighting all the code with your mouse, then hit Cmd+k or Ctrl+K.
   - Instruct it to change the game in some way (e.g. add inline styles, add a start screen, make it 4x4 instead of 3x3)
 
 Step 3: Hit Cmd+L or Ctrl+L and ask the chat what the code does
@@ -14,23 +14,111 @@ Step 3: Hit Cmd+L or Ctrl+L and ask the chat what the code does
 Step 4: To try out cursor on your own projects, go to the file menu (top left) and open a folder.
 */
 
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
+  const [start, setStart] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+    <div className="App" style={{ textAlign: "center" }}>
+      <header className="App-header">Tic Tac Toe</header>
+      {!start ? (
+        <button onClick={() => setStart(true)}>Start Game</button>
+      ) : (
+        <TicTacToe />
+      )}
     </div>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+function TicTacToe() {
+  const [board, setBoard] = useState(Array(16).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  function handleClick(i) {
+    const boardCopy = [...board];
+    if (winner || boardCopy[i]) return;
+    boardCopy[i] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXIsNext(!xIsNext);
+  }
+
+  function renderSquare(i) {
+    return (
+      <button
+        onClick={() => handleClick(i)}
+        style={{ width: "50px", height: "50px" }}
+      >
+        {board[i]}
+      </button>
+    );
+  }
+
+  return (
+    <div>
+      <div className="status">
+        {winner ? "Winner: " + winner : "Next Player: " + (xIsNext ? "X" : "O")}
+      </div>
+      <div>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+        {renderSquare(3)}
+      </div>
+      <div>
+        {renderSquare(4)}
+        {renderSquare(5)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+      </div>
+      <div>
+        {renderSquare(8)}
+        {renderSquare(9)}
+        {renderSquare(10)}
+        {renderSquare(11)}
+      </div>
+      <div>
+        {renderSquare(12)}
+        {renderSquare(13)}
+        {renderSquare(14)}
+        {renderSquare(15)}
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+    [0, 5, 10, 15],
+    [3, 6, 9, 12],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c, d] = lines[i];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c] &&
+      squares[a] === squares[d]
+    ) {
+      return squares[a];
+    }
+  }
+  return null;
+}
--- projects/python/main.py
+++ projects/python/main.py
@@ -2,7 +2,7 @@
 # Welcome to Cursor #
 #####################
 
-'''
+"""
 Step 1: Try generating with Cmd+K or Ctrl+K on a new line. Ask for CLI-based game of TicTacToe.
 
 Step 2: Hit Cmd+L or Ctrl+L and ask the chat what the code does.
@@ -12,34 +12,58 @@ Step 3: Try highlighting all the code with your mouse, then hit Cmd+k or Ctrl+K.
    - Instruct it to change the game in some way (e.g. add colors, add a start screen, make it 4x4 instead of 3x3)
 
 Step 4: To try out cursor on your own projects, go to the file menu (top left) and open a folder.
-''''
+"""
+
+
+import os
+from termcolor import colored
+
 
 def print_board(board):
     for row in board:
-        print(" ".join(row))
+        print(
+            " ".join(
+                [
+                    colored(cell, "red")
+                    if cell == "X"
+                    else colored(cell, "blue")
+                    if cell == "O"
+                    else cell
+                    for cell in row
+                ]
+            )
+        )
+
 
 def check_win(board):
     for row in board:
-        if row.count(row[0]) == len(row) and row[0] != ' ':
+        if row.count(row[0]) == len(row) and row[0] != " ":
             return True
 
     for col in range(len(board)):
         check = []
         for row in board:
             check.append(row[col])
-        if check.count(check[0]) == len(check) and check[0] != ' ':
+        if check.count(check[0]) == len(check) and check[0] != " ":
             return True
 
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+    if board[0][0] == board[1][1] == board[2][2] == board[3][3] and board[0][0] != " ":
         return True
-    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+    if board[0][3] == board[1][2] == board[2][1] == board[3][0] and board[0][3] != " ":
         return True
 
     return False
 
+
 def tictactoe():
-    board = [[' ' for _ in range(3)] for _ in range(3)]
-    player = 'X'
+    os.system("clear")
+    print("Welcome to Tic Tac Toe!")
+    print("Player X is red and Player O is blue.")
+    input("Press Enter to start...")
+    os.system("clear")
+
+    board = [[" " for _ in range(4)] for _ in range(4)]
+    player = "X"
 
     while True:
         print_board(board)
@@ -47,7 +71,7 @@ def tictactoe():
         row = int(input("Enter row: "))
         col = int(input("Enter column: "))
 
-        if board[row][col] != ' ':
+        if board[row][col] != " ":
             print("Invalid move, try again.")
             continue
 
@@ -57,7 +81,7 @@ def tictactoe():
             print("Player", player, "wins!")
             break
 
-        player = 'O' if player == 'X' else 'X'
+        player = "O" if player == "X" else "X"
+
 
 tictactoe()
-
